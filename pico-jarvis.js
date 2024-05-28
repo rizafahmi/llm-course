aefaaa53a148436a0b64b7bd932273d0bbc84cb1
Riza Fahmi <rizafahmi@gmail.com>
2024-05-28 21:29:11 +0700

    Chain of Thought

--- pico-jarvis.js
+++ pico-jarvis.js
@@ -1,7 +1,40 @@
 import http from 'http';
 import fs from 'fs';
 
-const LLAMA_API_URL = process.env.LLAMA_API_URL || 'http://13.229.203.255:11434/api/generate';
+const LLAMA_API_URL = process.env.LLAMA_API_URL || 'http://127.0.0.1:11434/api/generate';
+
+const SYSTEM_MESSAGE = `You run in a process of Question, Thought, Action, Observation.
+
+Use Thought to describe your thoughts about the question you have been asked.
+Observation will be the result of running those actions.
+
+If you can not answer the question from your memory, use Action to run one of these actions available to you:
+
+- exchange: from to
+- lookup: terms
+Here are some sample sessions.
+
+Question: What is capital of france?
+Thought: This is about geography, I can recall the answer from my memory.
+Action: lookup: capital of France.
+Observation: Paris is the capital of France.
+Answer: The capital of France is Paris.
+
+Question: Who painted Mona Lisa?
+Thought: This is about general knowledge, I can recall the answer from my memory.
+Action: lookup: painter of Mona Lisa.
+Observation: Mona Lisa was painted by Leonardo da Vinci .
+Answer: Leonardo da Vinci painted Mona Lisa.
+
+Question: What is the exchange rate from USD to EUR?
+Thought: This is about currency exchange rates, I need to check the current rate.
+Action: exchange: USD EUR
+Observation: 0.8276 EUR for 1 USD.
+Answer: The current exchange rate is 0.8276 EUR for 1 USD.
+
+Let's go!
+
+`;
 
 async function llama(question) {
   const method = 'POST';
@@ -9,7 +42,7 @@ async function llama(question) {
     'Content-Type': 'application/json'
   };
   const body = JSON.stringify({
-    model: 'orca-mini',
+    model: 'mistral-openorca',
     prompt: question,
     options: {
       num_predict: 200,
@@ -25,12 +58,71 @@ async function llama(question) {
   return response.trim();
 }
 
-function createPrompt(question) {
-  return `This is a conversation between User and Pico Jarvis, a friendly chatbot. Pico Jarvis is helpful, kind, honest, and never fails to answer any requests immediately, with precision, and concisely in 10 words or less.
-  User: ${question}
-  Pico Jarvis:
+async function reason(inquiry) {
+  const prompt = SYSTEM_MESSAGE + "\n\n" + inquiry;
+  const response = await llama(prompt);
 
-  `
+  console.log(`--------------\n${response}\n--------------`)
+  
+  let conclusion = "";
+
+  const action = await act(response);
+  if (action === null) {
+    return answer(response);
+  } else {
+    conclusion = await llama(finalPrompt(inquiry, action.result));
+  }
+
+  return conclusion;
+}
+
+async function act(text) {
+  const MARKER = "Action:";
+  const pos = text.lastIndexOf(MARKER);
+  if (pos < 0) return null;
+
+  const subtext = text.substr(pos) + "\n";
+  const matches = /Action:\s*(.*?)\n/.exec(subtext);
+  const action = matches[1];
+  if (!action) return null;
+
+  const SEPARATOR = ":";
+  const sep = action.indexOf(SEPARATOR);
+  if (sep < 0) return null;
+
+  const name = action.substring(0, sep);
+  const args = action.substring(sep + 1).trim().split(" ");
+
+  if (name === "lookup") return null;
+  if (name === "exchange") {
+    const result = await exchange(args[0].trim(), args[1].trim());
+    console.log("ACT Exchange", { args, result });
+    return { action, name, args, result };
+  }
+  console.error("Not recognized action", { name, args });
+  return null;
+}
+function finalPrompt(inquiry, observation) {
+  return `${inquiry}
+  Observation: ${observation}
+  Thought: Now I have the answer.
+  Answer:`;
+} 
+
+async function exchange(from, to) {
+  const url = `https://open.er-api.com/v6/latest/${from}`;
+  console.log("Fetching ", url);
+  const response = await fetch(url);
+  const data = await response.json();
+  const rate = data.rates[to];
+  return `As per ${data.time_last_update_utc}, 1 ${from} equal to ${Math.ceil(rate)} ${from}.`;
+}
+async function answer(text) {
+  const MARKER = "Answer:";
+  const pos = text.lastIndexOf(MARKER);
+  if (pos < 0) return "?";
+  const answer = text.substr(pos + MARKER.length).trim();
+  return answer;
 }
 
 async function handler(req, res) {
@@ -45,7 +137,7 @@ async function handler(req, res) {
     const parsedUrl = new URL(`http://localhost${url}`);
     const { search } = parsedUrl;
     const question = decodeURIComponent(search.substring(1));
-    const answer = await llama(createPrompt(question));
+    const answer = await reason(`Question: ${question}`);
     console.log({ question, answer });
     res.writeHead(200).end(answer);
   } else {
